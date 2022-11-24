5f1b1125a32ff4d7d499b4a2dc120343e3bdd516
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-11-24 15:45:26 +0600

    banner updated

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -4,23 +4,23 @@ const Banner = () => {
     // const car
     return (
 
-        <div className="carousel w-full">
+        <div className="carousel w-full my-10">
             <div id="slide1" className="carousel-item relative w-full ">
-                <img src="https://hiphopmakers.b-cdn.net/wp-content/uploads/2022/09/Best-Websites-To-Buy-Used-Musical-Instruments.webp" alt="music_gear" className="lg:w-4/5 mx-auto rounded-2xl " style={{ "height": "80vh" }} />
+                <img src="https://hiphopmakers.b-cdn.net/wp-content/uploads/2022/09/Best-Websites-To-Buy-Used-Musical-Instruments.webp" alt="music_gear" className="w-full lg:w-4/5 mx-auto rounded-2xl h-96 lg:h-[80vh] " />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide3" className="btn btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full ">
-                <img src="https://www.soundpure.com/img/Mobile-Vintage-Used-Banner.jpg" alt="music_gear" className="lg:w-4/5 mx-auto rounded-2xl" style={{ "height": "80vh" }} />
+                <img src="https://preview.redd.it/4cvro0z210r41.jpg?width=640&crop=smart&auto=webp&s=c4d13fd0129125bc44af4e7b263cdef69d03c65b" alt="music_gear" className="w-full lg:w-4/5 mx-auto rounded-2xl h-96 lg:h-[80vh]" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full ">
-                <img src="https://images.squarespace-cdn.com/content/v1/5a90e4c80dbda346e74ce9da/1627916001547-VVD7TT493ZGCMS8WQ8J8/1+-+Why+No+Instruments+Title.png" alt="music_gear" className="lg:w-4/5 mx-auto rounded-2xl" style={{ "height": "80vh" }} />
+                <img src="https://instagrammernews.com/image/BIa0y5rAojb" alt="music_gear" className="w-full lg:w-4/5 mx-auto rounded-2xl h-96 lg:h-[80vh]" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
